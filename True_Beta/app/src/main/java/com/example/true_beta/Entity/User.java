404b17b5777f9c1e957fe26d9166f6dcf88c6696
Scp-999 <995151080@qq.com>
2021-11-03 15:28:24 +0800

    2021/11/3

--- True_Beta/app/src/main/java/com/example/true_beta/Entity/User.java
+++ True_Beta/app/src/main/java/com/example/true_beta/Entity/User.java
@@ -1,4 +1,31 @@
 package com.example.true_beta.Entity;
 
 public class User {
+    private String UserName;
+    private String PassWord;
+    private String PhoneNum;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassWord() {
+        return PassWord;
+    }
+
+    public void setPassWord(String passWord) {
+        PassWord = passWord;
+    }
+
+    public String getPhoneNum() {
+        return PhoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        PhoneNum = phoneNum;
+    }
 }
